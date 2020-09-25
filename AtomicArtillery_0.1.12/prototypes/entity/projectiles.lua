b36663c5f6946ea7013e27dd8533585420f45110
James Guillochon <guillochon@gmail.com>
2020-09-25 10:43:57 -0400

    Use new atomic bomb effect

--- AtomicArtillery_0.1.12/prototypes/entity/projectiles.lua
+++ AtomicArtillery_0.1.12/prototypes/entity/projectiles.lua
@@ -32,76 +32,7 @@ data:extend(
 			priority = "high",
 			scale = 0.25,
 		},
-		action =
-		{
-			type = "direct",
-			action_delivery =
-			{
-				type = "instant",
-				target_effects =
-				{
-					{
-						repeat_count = 100,
-						type = "create-trivial-smoke",
-						smoke_name = "nuclear-smoke",
-						offset_deviation = {{-1, -1}, {1, 1}},
-						slow_down_factor = 1,
-						starting_frame = 3,
-						starting_frame_deviation = 5,
-						starting_frame_speed = 0,
-						starting_frame_speed_deviation = 5,
-						speed_from_center = 0.5,
-						speed_deviation = 0.2
-					},
-					{
-						type = "create-entity",
-						entity_name = "explosion"
-					},
-					{
-						type = "damage",
-						damage = {amount = 1500, type = "explosion"}
-					},
-					{
-						type = "create-entity",
-						entity_name = "small-scorchmark",
-						check_buildability = true
-					},
-					{
-						type = "nested-result",
-						action =
-						{
-							type = "area",
-							target_entities = false,
-							trigger_from_target = true,
-							repeat_count = 4000,
-							radius = 50,
-							action_delivery =
-							{
-								type = "projectile",
-								projectile = "atomic-artillery-wave",
-								starting_speed = 0.5
-							},
-						}
-					}
-				}
-			}
-		},
-		final_action =
-		{
-			type = "direct",
-			action_delivery =
-			{
-				type = "instant",
-				target_effects =
-				{
-					{
-						type = "create-entity",
-						entity_name = "small-scorchmark",
-						check_buildability = true
-					}
-				}
-			}
-		},
+		action = "atomic-bomb",
 		animation =
 		{
 			filename = "__base__/graphics/entity/bullet/bullet.png",
